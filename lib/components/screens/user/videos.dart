ff9a6fe2065cd7dca44d059357d807159b238759
Micah Elijah <melijah200@gmail.com>
2022-11-30 22:50:00 +0100

    Pull down to refresh videos

--- lib/components/screens/user/videos.dart
+++ lib/components/screens/user/videos.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:smartrr/components/screens/user/video_list_item.dart';
 import 'package:smartrr/models/video.dart';
@@ -11,65 +12,94 @@ class Videos extends StatefulWidget {
 }
 
 class _VideosState extends State<Videos> {
+  StreamController<List<Video>> _streamController;
+
+  loadVideos() async {
+    try {
+      List<Video> videos = await VideoService.getAllVideos();
+      _streamController.add(videos);
+      return videos;
+    } catch (e) {
+      _streamController.addError(e);
+    }
+  }
+
+  Future<Null> _handleRefresh() async {
+    try {
+      List<Video> videos = await VideoService.getAllVideos();
+      _streamController.add(videos);
+      return null;
+    } catch (e) {
+      _streamController.addError(e);
+    }
+  }
+
+  Widget _errorHandler(Object error) {
+    switch (error.runtimeType.toString()) {
+      case "_ClientSocketException":
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Network Error",
+                  style: TextStyle().copyWith(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Text(
+                  "Please check that you have an active internet connection",
+                  textAlign: TextAlign.center,
+                )),
+              ],
+            ),
+          ],
+        );
+      default:
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Something is not right :(",
+              style: TextStyle()
+                  .copyWith(fontWeight: FontWeight.w500, fontSize: 18),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Text(
+                  "We encountered an error while fetching your videos",
+                  textAlign: TextAlign.center,
+                )),
+              ],
+            ),
+          ],
+        );
+    }
+  }
+
+  @override
+  void initState() {
+    _streamController = new StreamController();
+    loadVideos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget _errorHandler(Object error) {
-      switch (error.runtimeType.toString()) {
-        case "_ClientSocketException":
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Network Error",
-                    style: TextStyle().copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Text(
-                    "Please check that you have an active internet connection",
-                    textAlign: TextAlign.center,
-                  )),
-                ],
-              ),
-            ],
-          );
-        default:
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Something is not right :(",
-                style: TextStyle()
-                    .copyWith(fontWeight: FontWeight.w500, fontSize: 18),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Text(
-                    "We encountered an error while fetching your videos",
-                    textAlign: TextAlign.center,
-                  )),
-                ],
-              ),
-            ],
-          );
-      }
-    }
-
     final screenWidth = MediaQuery.of(context).size.width;
     final colCount = screenWidth ~/ 200;
 
@@ -89,20 +119,33 @@ class _VideosState extends State<Videos> {
     }
 
     return StreamBuilder(
-      stream: VideoService.getAllVideos().asStream(),
-      builder: (context, AsyncSnapshot<List<Video>> snapshot) =>
-          snapshot.hasError
-              ? _errorHandler(snapshot.error)
-              : Container(
-                  child: snapshot.hasData
-                      ? Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: _videos(snapshot.data),
-                        )
-                      : Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                ),
-    );
+        stream: _streamController.stream,
+        builder: (context, AsyncSnapshot<List<Video>> snapshot) {
+          if (snapshot.hasError) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _errorHandler(snapshot.error),
+                TextButton.icon(
+                  onPressed: _handleRefresh,
+                  icon: Icon(Icons.refresh),
+                  label: Text("Refresh"),
+                )
+              ],
+            );
+          }
+
+          if (snapshot.hasData) {
+            return RefreshIndicator(
+              onRefresh: _handleRefresh,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: _videos(snapshot.data),
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
