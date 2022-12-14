337414e98b58e32f1ac2d5efc34bd3d3a19e1768
Micah Elijah <melijah200@gmail.com>
2022-12-14 13:51:10 +0100

    Add product items checklist functionality

--- lib/components/screens/user/product_details.dart
+++ lib/components/screens/user/product_details.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:smartrr/models/product.dart';
 import 'package:smartrr/utils/colors.dart';
+import 'package:smartrr/utils/utils.dart';
 
 class ProductDetails extends StatefulWidget {
   final Product product;
@@ -11,19 +12,140 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+  late double productPrice = widget.product.price;
+  late List<ProductItemWithCheckbox> productItems;
+  late List<ProductItemWithCheckbox> selectedItems;
+
+  void _handleCheckboxChange(bool? val, ProductItemWithCheckbox item) {
+    int itemIndex = productItems.indexOf(item);
+    if (selectedItems.length <= 2 && !val!) {
+      showToast(msg: "A least two items must be selected");
+    } else {
+      productItems[itemIndex].isSelected = val!;
+
+      if (!val) {
+        selectedItems.remove(item);
+        productPrice -= item.price;
+      } else {
+        selectedItems.add(item);
+        productPrice += item.price;
+      }
+      setState(() {});
+    }
+  }
+
+  Widget productItemTile(ProductItemWithCheckbox item) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                item.item,
+                style: TextStyle().copyWith(fontWeight: FontWeight.w600),
+              ),
+              Checkbox(
+                onChanged: (val) {
+                  _handleCheckboxChange(val, item);
+                },
+                value: item.isSelected,
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Text("N${item.price}"),
+                ],
+              ),
+              item.isSelected ? itemQuantitySelector(item) : SizedBox(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void incrementItemQuantity(ProductItemWithCheckbox item) {
+    int itemIndex = selectedItems.indexOf(item);
+    selectedItems[itemIndex].quantity++;
+    setState(() {});
+  }
+
+  void decrementItemQuantity(ProductItemWithCheckbox item) {
+    int itemIndex = selectedItems.indexOf(item);
+    selectedItems[itemIndex].quantity--;
+    setState(() {});
+  }
+
+  Widget itemQuantitySelector(ProductItemWithCheckbox item) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(
+          onPressed:
+              item.quantity > 1 ? () => decrementItemQuantity(item) : null,
+          color: primaryColor,
+          icon: Icon(
+            Icons.remove,
+            size: 20,
+          ),
+        ),
+        // GestureDetector(
+        //   onTap: () => item.quantity > 1 ? decrementItemQuantity(item) : null,
+        //   child: Container(
+        //     padding: EdgeInsets.zero,
+        //     decoration: BoxDecoration(
+        //       color: primaryColor,
+        //       borderRadius: BorderRadius.circular(5.0),
+        //     ),
+        //     child: Center(
+        //       child: Icon(
+        //         Icons.remove,
+        //         size: 20,
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        SizedBox(width: 5.0),
+        Text(item.quantity.toString()),
+        SizedBox(width: 5.0),
+        IconButton(
+          onPressed:
+              item.quantity < 10 ? () => incrementItemQuantity(item) : null,
+          color: primaryColor,
+          icon: Icon(
+            Icons.add,
+            size: 20,
+          ),
+        ),
+        // GestureDetector(
+        //   onTap: () => item.quantity < 20 ? incrementItemQuantity(item) : null,
+        //   child: Container(
+        //     padding: EdgeInsets.zero,
+        //     decoration: BoxDecoration(
+        //       color: primaryColor,
+        //       borderRadius: BorderRadius.circular(5.0),
+        //     ),
+        //     child: Center(
+        //       child: Icon(
+        //         Icons.add,
+        //         size: 20,
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    double productPrice = widget.product.price;
-
-    List<ProductItemWithCheckbox> _productItems = widget.product.items!
-        .map((item) => ProductItemWithCheckbox(
-            item: item.item, price: item.price, quantity: item.quantity))
-        .toList();
-
-    _handleCheckboxChange(bool? val) {
-      return !val!;
-    }
-
     return Scaffold(
       appBar: AppBar(title: Text(widget.product.name)),
       body: SingleChildScrollView(
@@ -74,7 +196,7 @@ class _ProductDetailsState extends State<ProductDetails> {
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 5.0),
                     child: Text(
-                      "N$productPrice",
+                      "N${productPrice}",
                       style: TextStyle().copyWith(
                         color: primaryColor,
                         fontSize: 20,
@@ -121,30 +243,43 @@ class _ProductDetailsState extends State<ProductDetails> {
                         )
                       : SizedBox(),
                   Column(
-                    children: widget.product.items!
-                        .map(
-                          (item) => ListTile(
-                            title: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(item.item),
-                                Checkbox(
-                                  onChanged: _handleCheckboxChange,
-                                  value: true,
-                                ),
-                              ],
-                            ),
-                            dense: true,
-                            contentPadding: EdgeInsets.zero,
-                            selectedColor: darkGrey,
-                            textColor: lightGrey,
-                            selected: true,
-                            subtitle: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [Text("N${item.price}"), Text("1")],
-                            ),
-                          ),
-                        )
+                    children: productItems
+                        .map((item) => productItemTile(item)
+                            //     ListTile(
+                            //   title: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            //     children: [
+                            //       Text(
+                            //         item.item,
+                            //         style: TextStyle()
+                            //             .copyWith(fontWeight: FontWeight.w600),
+                            //       ),
+                            //       Checkbox(
+                            //         onChanged: (val) {
+                            //           _handleCheckboxChange(val, item);
+                            //         },
+                            //         value: item.isSelected,
+                            //       ),
+                            //     ],
+                            //   ),
+                            //   dense: true,
+                            //   contentPadding: EdgeInsets.zero,
+                            //   selectedColor: darkGrey,
+                            //   textColor: lightGrey,
+                            //   selected: true,
+                            //   subtitle: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            //     children: [
+                            //       Row(
+                            //         children: [
+                            //           Text("N${item.price}"),
+                            //         ],
+                            //       ),
+                            //       itemQuantitySelector(item)
+                            //     ],
+                            //   ),
+                            // ),
+                            )
                         .toList(),
                   )
                 ],
@@ -154,5 +289,15 @@ class _ProductDetailsState extends State<ProductDetails> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    productItems = widget.product.items!
+        .map((item) => ProductItemWithCheckbox(
+            item: item.item, price: item.price, quantity: item.quantity))
+        .toList();
+    selectedItems = productItems.sublist(0);
+    super.initState();
   }
 }
